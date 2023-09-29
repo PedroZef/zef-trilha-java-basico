e3c4ef05ccc8bf05af4730b80895d883ac92251c
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-09-29 17:11:10 -0300

    Criando a Conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Obter pela scanner os valores digitados no terminal/
+        Scanner input = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+         //Exibir as mensagens para nosso usuário?
+        System.out.print("Digite o número da conta: ");
+        numero = Integer.parseInt(input.nextLine());
+
+        System.out.print("Digite o nome da agência: ");
+        agencia = input.next();
+
+        System.out.print("Digite seu nome: ");
+        nomeCliente = input.nextLine();
+
+        System.out.print("Digite seu saldo: ");
+        saldo = input.nextDouble();
+
+
+        //Exibir a mensagem da conta criada/
+        System.out.printf(
+            "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s" +
+            ", conta %d e seu saldo %.2f já está disponível para saque. Entre em contato e estamos à disposição para ajudar!.",
+            nomeCliente, agencia, numero, saldo);
+
+        input.close();
     }
+
 }
+   
+         
+ 
+        
+       
+
+
+
