dda3e669f038f4f5ba5025c49dfacd6ca8e7bca1
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-10-03 14:49:49 -0300

    Update no ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -11,7 +11,7 @@ public class ContaTerminal {
         String nomeCliente;
         double saldo;
 
-         //Exibir as mensagens para nosso usuário?
+         //Exibir as mensagens para nosso usuário
         System.out.print("Digite o número da conta: ");
         numero = Integer.parseInt(input.nextLine());
 
@@ -25,11 +25,11 @@ public class ContaTerminal {
         saldo = input.nextDouble();
 
 
-        //Exibir a mensagem da conta criada/
+        //Exibir a mensagem da conta criada
         System.out.printf(
             "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s" +
-            ", conta %d e seu saldo %.2f já está disponível para saque." +
-             " Entre em contato e estamos à disposição para ajudar!.",
+            ", conta %d e seu saldo %.2f." + "Já está disponível para saque." +
+             " Entre em contato conosco e estamos à disposição para ajudar!.",
             nomeCliente, agencia, numero, saldo);
 
         input.close();
